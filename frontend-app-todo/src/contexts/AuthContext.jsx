0602c91b590f93b2c20ad7eff8bf07876e98eaeb
Alexandre Mariano <86934710+devmariano@users.noreply.github.com>
2023-10-03 19:48:16 -0300

    criado context de autenticação

---
+++ frontend-app-todo/src/contexts/AuthContext.jsx
@@ -0,0 +1,24 @@
+import PropTypes from "prop-types";
+import { createContext, useState } from "react";
+// este estão os dados
+export const AuthContext = createContext({
+  auth: false,
+  user: {},
+  setAuth: () => {},
+  setUser: () => {},
+});
+
+//este prove os dados
+export const AuthProvider = ({ children }) => {
+  const [auth, setAuth] = useState(false);
+  const [user, setUser] = useState(undefined);
+  return (
+    <AuthContext.Provider value={{ auth, user, setAuth, setUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+AuthProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
