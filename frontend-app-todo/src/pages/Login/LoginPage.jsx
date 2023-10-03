3319ebcabb9fe0d91a0b4b970546e51ebbe642d8
Alexandre Mariano <86934710+devmariano@users.noreply.github.com>
2023-10-03 15:21:55 -0300

    implementado formulario de login

--- frontend-app-todo/src/pages/Login/LoginPage.jsx
+++ frontend-app-todo/src/pages/Login/LoginPage.jsx
@@ -1,5 +1,41 @@
+import { useForm } from "react-hook-form";
+import InputMask from "react-input-mask";
+
 export const LoginPage = () => {
+  const { handleSubmit, register, formState: {errors} } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
-    <div>LoginPage</div>
-  )
-}
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label htmlFor="login">Login</label>
+          <InputMask
+            mask="999.999.999-99"
+            id="login"
+            placeholder="Digite seu login"
+            {...register("login", {
+              required: "Campo de login tem preenchimento obrigatório"
+            })}
+          />
+          {errors?.login && <p>{errors?.login?.message}</p>}
+        </div>
+        <div>
+          <label htmlFor="pass">Senha</label>
+          <input type="password" id="pass" {...register("pass", {
+              required: "Campo de senha tem preenchimento obrigatório",
+              minLength: {
+                value: 8,
+                message: "Campo senha deve possuir no mínimo 8 caracteres"
+              }
+            })} />
+            {errors?.pass && <p>{errors?.pass?.message}</p>}
+        </div>
+        <button type="submit">Entrar</button>
+      </form>
+    </div>
+  );
+};
